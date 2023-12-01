225cb4cbcc3a9e89e04ac86dbd33b85f82e27f7f
safarboyalkhwarizmi <safarboyalkhwarizmi@gmail.com>
2023-12-01 16:56:13 +0500

    I make cors configuration public!

--- java_warehouse/src/main/java/uz/jarvis/config/WebConfig.java
+++ java_warehouse/src/main/java/uz/jarvis/config/WebConfig.java
@@ -9,10 +9,29 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:50812", "http://localhost:4200")
+        .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
         .maxAge(3600);
   }
 }
+
+/*package uz.jarvis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:50812", "http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}*/
